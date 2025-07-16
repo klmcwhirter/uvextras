f334c5dcc769095f9e3fe7b8cb85abc728346b1e
Kevin McWhirter <klmcw@yahoo.com>
2025-07-16 09:24:27 -0700

    Adjust install.sh to not depend on uvextras

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function simple_create
+{
+    if [ ! -d .venv ]
+    then
+        uv venv --system-site-packages
+        uv sync "$@"
+    fi
+}
+
 rm -fr ${HOME}/.local/share/uvextras
 rm -fr ${HOME}/.config/uvextras
 
 if [ "$1" != "-u" ]
 then
-    ./bin/uvextras run allclean
+    rm -fr .venv
+    simple_create --no-dev
+    uv run --no-project python -m uvextras run allclean
 
     mkdir -p ${HOME}/.local/share/uvextras/bin/
     cp -p bin/uvextras ${HOME}/.local/share/uvextras/bin/
@@ -18,6 +29,8 @@ then
     mkdir -p ${HOME}/.config/uvextras/
     cp uvextras.yaml ${HOME}/.config/uvextras/
 
+    simple_create
+
     cd ${HOME}/.local/share/uvextras/
-    uvextras run create
+    simple_create --no-dev
 fi
--- scripts/create.py
+++ scripts/create.py
@@ -11,7 +11,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--with-system', default=False, action='store_true')
 args = parser.parse_args(sys.argv[1:])
 
-cmd = f'uv venv{" --system-site-packages" if args.with_system else ""}'
 
 subprocess.call('rm -fr .venv', shell=True, text=True)
+
+cmd = f'uv venv{" --system-site-packages" if args.with_system else ""}'
+subprocess.call(f'{cmd}', shell=True, text=True)
+
+cmd = 'uv sync'
 subprocess.call(f'{cmd}', shell=True, text=True)
