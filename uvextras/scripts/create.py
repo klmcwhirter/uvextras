d5097ec9aca10327860b85602eaff907c9e75170
Kevin McWhirter <klmcw@yahoo.com>
2025-11-12 12:49:18 -0800

    Add verbose option to create.py script

--- uvextras/scripts/create.py
+++ uvextras/scripts/create.py
@@ -1,5 +1,5 @@
 # /// script
-# requires-python = ">=3.14"
+# requires-python = "==3.14.*"
 # dependencies = []
 # ///
 
@@ -10,15 +10,23 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--with-system', default=False, action='store_true')
+parser.add_argument('-v', '--verbose', default=False, action='store_true')
 args = parser.parse_args(sys.argv[1:])
 
 # prevent warning that VIRTUAL_ENV is different
 del os.environ['VIRTUAL_ENV']
 
-subprocess.call('rm -fr .venv', shell=True, text=True)
-
-cmd = f'uv venv{" --system-site-packages" if args.with_system else ""}'
+cmd = 'rm -fr .venv'
+if args.verbose:
+    print(f'{cmd}')
 subprocess.call(f'{cmd}', shell=True, text=True)
 
-cmd = 'uv sync'
+cmd = f'uv venv{" --system-site-packages" if args.with_system else ""}'
+if args.verbose:
+    print(f'{cmd}')
+subprocess.call(f'{cmd}', shell=True, text=True)
+
+cmd = 'uv sync --frozen'
+if args.verbose:
+    print(f'{cmd}')
 subprocess.call(f'{cmd}', shell=True, text=True)
